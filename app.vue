9a59fbf7133108bb33fe49ed6d2325deb6a73ad7
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-02-05 16:18:54 -0500

    3 files modified

--- app.vue
+++ app.vue
@@ -1,43 +1,48 @@
 <template>
-  <div class="bg-slate-900 min-h-full h-screen text-slate-100">
+<div class="w-full bg-slate-900 min-x-full h-screen p-4">
+  <div class=" text-slate-100 max-w-4xl mx-auto">
   <theHeader />
-  <div class="p-2">
-  <label class="text-lg" for="theText">Paste your text</label>
-  <textarea v-model="message" placeholder="your text" class="bg-stone-700" name="theText"/>
-  <div class="grid-container">
-  <div class="grid-item">
-    <label for="theClass">Classss</label> 
-    <input v-model="myclass" name="theClass" class="bg-stone-700" />
+  <div class="text-xl py-4">
+  This is a description of the app.
   </div>
-  <div class="grid-item">
-    <label for="theID">ID (incremented)</label> 
-    <input v-model="myid" class="bg-stone-700" name="theID" />
-  </div>
-  <div class="grid-item">
-    <label for="theTag">Tag Type</label>
-    <select name="theTag" id="tag-select" v-model="tag" class="bg-stone-700">
-        <option value="">--Please choose an option--</option>
-        <option value="div">H1</option>
-        <option value="div">H2</option>
-        <option value="div">H3</option>
-        <option value="div">div</option>
-        <option value="p">p</option>
-        <option value="span">span</option>
-    </select>
-  </div>
-  <div class="grid-item">
-    <label class="form-control">
-      <input type="radio" name="radio" value="words" v-model="mode"/>
-      Wrap Words
-    </label>
-    <label class="form-control">
-      <input type="radio" name="radio" value="letters" v-model="mode" />
-      Wrap Letters
-    </label>
-  </div>
+  <div>
+    <label for="theText">Paste your text</label>
+    <textarea v-model="message" class="bg-stone-700 focus:border-solid focus:border-2 focus:border-teal-400 focus:ring-0 outline-none" name="theText"/>
+    <div class="grid-container">
+    <div class="grid-item">
+      <label for="theClass">Class</label> 
+      <input v-model="myclass" name="theClass" class="bg-stone-700 focus:border-solid focus:border-2 focus:border-teal-400 focus:ring-0 outline-none" />
+    </div>
+    <div class="grid-item">
+      <label for="theID">ID (incremented)</label> 
+      <input v-model="myid" class="bg-stone-700 focus:border-solid focus:border-2 focus:border-teal-400 focus:ring-0 outline-none" name="theID" />
+    </div>
+    <div class="grid-item">
+      <label for="theTag">Tag Type</label>
+      <select name="theTag" id="tag-select" v-model="tag" class="bg-stone-700 ">
+          <option value="div">H1</option>
+          <option value="div">H2</option>
+          <option value="div">H3</option>
+          <option value="div">div</option>
+          <option value="p">p</option>
+          <option value="span">span</option>
+      </select>
+    </div>
+  <label>Wrap Type
+  <div class="flex">
+    <div class="flex items-center mr-4">
+        <input id="inline-radio" type="radio" name="inline-radio-group" class="w-4 h-4 text-blue-100 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"  value="words" v-model="mode">
+        <label for="inline-radio" class="ml-2 text-sm font-medium text-gray-100 dark:text-gray-100">Words</label>
+    </div>
+    <div class="flex items-center mr-4">
+        <input id="inline-2-radio" type="radio" name="inline-radio-group" class="w-4 h-4 text-blue-100 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"  value="letters" v-model="mode">
+        <label for="inline-2-radio" class="ml-2 text-sm font-medium text-gray-100 dark:text-gray-100">Letters</label>
+    </div>
+</div>
+</label>
   </div>
   <div class="pt-6">
-    <label for="theOutput">Your Output!!!</label>
+    <label for="theOutput">Your Output</label>
     <code name="theOutput">
       <div class="myoutput bg-stone-700">
         <div v-if="mode === 'words'" ref="message" id="copiedText" >
@@ -68,7 +73,7 @@
       <span>Coming soonish!</span>
     </div>
   </div>
-  
+  </div>
   </div>
 </template>
 
--- components/theButton.vue
+++ components/theButton.vue
@@ -3,13 +3,12 @@
     @click="copyText"
     class="bg-teal-400 hover:bg-teal-500 px-3 py-2 font-bold"
   >
-    Copy Now
+    Copy
   </button>
 </template>
 
 <script setup>
 function copyText(mode) {
- 
   if (mode === 'words') {
     var copied = document.getElementById('copiedText');
     navigator.clipboard.writeText(copied.textContent);
