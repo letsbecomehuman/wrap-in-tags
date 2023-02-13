311fa05dfc57f34a470c3d57a7d22b21a8dcf56d
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-02-12 23:26:36 -0500

    1 file modified

--- components/theHeader.vue
+++ components/theHeader.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1 class="text-3xl font-bold py-3 px-2 text-center bg-stone-900 shadow-sm">
+<div class="bg-stone-900">
+  <h1 class="text-3xl font-bold py-6 px-2 text-center shadow-sm">
     <span class="text-teal-400">&lt;</span>
     WRAP IN TAGS
     <span class="text-teal-400">/&#62;</span>
   </h1>
+  </div>
 </template>
